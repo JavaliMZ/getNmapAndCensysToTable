ab9b1bac694f174f59e011d26f467425d666d5a0
JavaliMZ <syjulio123@gmail.com>
2024-08-04 08:14:46 +0100

    Committing changes

--- catNmapAndCensysToTable/__init__.py
+++ catNmapAndCensysToTable/__init__.py
@@ -4,153 +4,171 @@ import os
 import sys
 import re
 import json
-from tabulate import tabulate # type: ignore
-
-def is_relevant_info_from_line_nmap(line):
-    return not line.startswith('#') or 'Host' in line
-
-def extract_ip_and_ports_from_line_nmap(line):
-    
-    parts = line.split()
-    ip = parts[1]
-
-    if 'Ports:' not in line:
-        return ip, []
-
-    ports_info = line.split('Ports: ')[1]
-    ports = ports_info.split(',')
-
-    return ip, ports
-
-def port_details_from_nmap(ip, ports):
-    data = []
-    cve_pattern = re.compile(r'CVE-\d{4}-\d+', re.IGNORECASE)
-
-    for port_info in ports:
-        port_details = port_info.split('/')
-
-        if len(port_details) >= 7:
-            port, state, protocol, _, service, _, info = port_details[:7]
-
-            cves = cve_pattern.findall(info)
-            cve_string = ', '.join(cves) if cves else 'N/A'
-
-            data.append({
-                'IP': ip,
-                'Port': port,
-                'Protocol': protocol.lower(),
-                'State': state.lower(),
-                'Service': service.lower(),
-                'Info': info,
-                'CVEs': cve_string
-            })
-
-    return data
-
-def parse_grepable_nmap_line(line, data):
-    if not is_relevant_info_from_line_nmap(line):
-        return
-
-    ip, ports = extract_ip_and_ports_from_line_nmap(line)
-    if not ports:
-        return
-
-    data.extend(port_details_from_nmap(ip, ports))
+from tabulate import tabulate  # type: ignore
 
 
+class NmapParser:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.data = []
+        self.cve_pattern = re.compile(r'CVE-\d{4}-\d+', re.IGNORECASE)
 
-def parse_grepable_nmap_output(file_path):
-    data = []
-    cve_pattern = re.compile(r'CVE-\d{4}-\d+', re.IGNORECASE)
+    def parse_file(self):
+        with open(self.file_path, 'r') as file:
+            for line in file:
+                if self.is_relevant_info_from_line(line):
+                    self.parse_line(line)
+        return self.data
 
-    with open(file_path, 'r') as file:
-        for line in file:
-            parse_grepable_nmap_line(line, data)                                                                                                                
+    @staticmethod
+    def is_relevant_info_from_line(line):
+        return not line.startswith('#') or 'Host' in line
 
+    def parse_line(self, line):
+        ip, ports = self.extract_ip_and_ports(line)
+        if not ports:
+            return
 
-    return data
+        self.data.extend(self.port_details(ip, ports))
 
-def parse_censys_json(file_path):
-    data = []
+    @staticmethod
+    def extract_ip_and_ports(line):
+        parts = line.split()
+        ip = parts[1]
 
-    try:
-        with open(file_path, 'r') as file:
-            json_data = json.load(file)
+        if 'Ports:' not in line:
+            return ip, []
 
-        for entry in json_data:
-            ip = entry.get('ip', 'N/A')
-            for service in entry.get('services', []):
-                port = service.get('port', 'N/A')
-                protocol = service.get('transport_protocol', 'N/A')
-                extended_service_name = service.get('extended_service_name', 'N/A')
+        ports_info = line.split('Ports: ')[1]
+        ports = ports_info.split(',')
+
+        return ip, ports
+
+    def port_details(self, ip, ports):
+        data = []
+        for port_info in ports:
+            port_details = port_info.split('/')
+
+            if len(port_details) >= 7:
+                port, state, protocol, _, service, _, info = port_details[:7]
+
+                cves = self.cve_pattern.findall(info)
+                cve_string = ', '.join(cves) if cves else 'N/A'
 
                 data.append({
                     'IP': ip,
                     'Port': port,
                     'Protocol': protocol.lower(),
-                    'State': 'N/A',
-                    'Service': extended_service_name.lower(),
-                    'Info': "",
-                    'CVEs': 'N/A'
+                    'State': state.lower(),
+                    'Service': service.lower(),
+                    'Info': info,
+                    'CVEs': cve_string
                 })
-    except FileNotFoundError:
-        print(f"File not found: {file_path}", file=sys.stderr)
-    except json.JSONDecodeError as e:
-        print(f"Error decoding JSON: {e}", file=sys.stderr)
-    except IOError as e:
-        print(f"Error reading file: {e}", file=sys.stderr)
 
-    return data
+        return data
 
-def print_table(data):
-    has_cves = any('CVEs' in entry and entry['CVEs'] != 'N/A' for entry in data)
-    headers = ['IP', 'Port', 'Protocol', 'State', 'Service', 'Info']
 
-    if has_cves:
-        headers.append('CVEs')
+class CensysParser:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.data = []
 
-    table = [
-        [line['IP'], line['Port'], line['Protocol'], line['State'],
-         line['Service'], line['Info']] + ([line['CVEs']] if has_cves else []) for line in data
-    ]
-    # print(tabulate(table, headers=headers, tablefmt='simple', numalign="left"))
-    print(tabulate(table, tablefmt='plain', numalign="left"))
+    def parse_file(self):
+        try:
+            with open(self.file_path, 'r') as file:
+                json_data = json.load(file)
 
-def get_files_paths_to_parse():
-    current_directory = os.getcwd()
+            for entry in json_data:
+                ip = entry.get('ip', 'N/A')
+                for service in entry.get('services', []):
+                    port = service.get('port', 'N/A')
+                    protocol = service.get('transport_protocol', 'N/A')
+                    extended_service_name = service.get('extended_service_name', 'N/A')
 
-    files_paths_to_parse = []
-    for file in os.listdir(current_directory):
-        if file.endswith('.gnmap') or file.endswith('.json'):
-            files_paths_to_parse.append(os.path.join(current_directory, file))
-    
-    if not files_paths_to_parse:
-        print("No files found to parse.", file=sys.stderr)
-        sys.exit(1)
-        
-    return files_paths_to_parse
+                    self.data.append({
+                        'IP': ip,
+                        'Port': port,
+                        'Protocol': protocol.lower(),
+                        'State': 'N/A',
+                        'Service': extended_service_name.lower(),
+                        'Info': "",
+                        'CVEs': 'N/A'
+                    })
 
-def get_all_data_from_files(files_paths_to_parse):
-    all_data = []
-    for file_path in files_paths_to_parse:
-        if file_path.endswith('.gnmap'):
-            file_data = parse_grepable_nmap_output(file_path)
-            all_data.extend(file_data)
-        elif file_path.endswith('.json'):
-            file_data = parse_censys_json(file_path)
-            all_data.extend(file_data)
+        except FileNotFoundError:
+            print(f"File not found: {self.file_path}", file=sys.stderr)
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}", file=sys.stderr)
+        except IOError as e:
+            print(f"Error reading file: {e}", file=sys.stderr)
 
-    return all_data
+        return self.data
 
-def main():
-    files_paths_to_parse = get_files_paths_to_parse()
-    all_data = get_all_data_from_files(files_paths_to_parse)
 
-    if not all_data:
-        print("No data found to display.", file=sys.stderr)
-        sys.exit(1)
+class FileManager:
+    @staticmethod
+    def get_files_paths_to_parse():
+        current_directory = os.getcwd()
 
-    print_table(all_data)
+        files_paths_to_parse = []
+        for file in os.listdir(current_directory):
+            if file.endswith('.gnmap') or file.endswith('.json'):
+                files_paths_to_parse.append(os.path.join(current_directory, file))
+
+        if not files_paths_to_parse:
+            print("No files found to parse.", file=sys.stderr)
+            sys.exit(1)
+
+        return files_paths_to_parse
+
+
+class ReportGenerator:
+    def __init__(self, all_data):
+        self.all_data = all_data
+
+    def print_table(self):
+        has_cves = any('CVEs' in entry and entry['CVEs'] != 'N/A' for entry in self.all_data)
+        headers = ['IP', 'Port', 'Protocol', 'State', 'Service', 'Info']
+
+        if has_cves:
+            headers.append('CVEs')
+
+        table = [
+            [line['IP'], line['Port'], line['Protocol'], line['State'],
+             line['Service'], line['Info']] + ([line['CVEs']] if has_cves else []) for line in self.all_data
+        ]
+        print(tabulate(table, tablefmt='plain', numalign="left"))
+
+
+class MainController:
+    def __init__(self):
+        self.file_paths = FileManager.get_files_paths_to_parse()
+
+    def get_all_data_from_files(self):
+        all_data = []
+        for file_path in self.file_paths:
+            if file_path.endswith('.gnmap'):
+                parser = NmapParser(file_path)
+                file_data = parser.parse_file()
+                all_data.extend(file_data)
+            elif file_path.endswith('.json'):
+                parser = CensysParser(file_path)
+                file_data = parser.parse_file()
+                all_data.extend(file_data)
+
+        return all_data
+
+    def run(self):
+        all_data = self.get_all_data_from_files()
+
+        if not all_data:
+            print("No data found to display.", file=sys.stderr)
+            sys.exit(1)
+
+        report_generator = ReportGenerator(all_data)
+        report_generator.print_table()
+
 
 if __name__ == "__main__":
-    main()
+    main_controller = MainController()
+    main_controller.run()
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='catNmapAndCensysToTable',
-    version='0.0.2',
+    version='0.1.0',
     description='Private package to catNmapAndCensysToTable, a tool to convert Nmap and Censys scans to a table',
     url='git@github.com:JavaliMZ/catNmapAndCensysToTable.git',
     author='Sylvain Júlio',
